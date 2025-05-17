8dc31471aaec1d53207b1af9a53f3a4efb97ef8f
Jobs Hiring <benjaminrenkema78@gmail.com>
2025-05-17 16:12:40 +0200

    The create table update Commit

--- db/supabase/create_table.sql
+++ db/supabase/create_table.sql
@@ -8,22 +8,22 @@ CREATE TABLE IF NOT EXISTS public.submit (
   status integer default 0,
   CONSTRAINT submit_pkey PRIMARY KEY (id)
 ) TABLESPACE pg_default;
--- 查询submit表，按提交时间，status=0（未处理），按照is_feature desc, created_at asc
--- 得到结果后，写入web_navigation，同时修改submit的status=1
+-- submit，status=0 ，is_feature desc, created_at asc
+-- web_navigation，submit status=1
 
 create table if not exists public.navigation_category (
     id bigint primary key generated always as identity,
     name text not null,
     title text,
     sort integer,
-    del_flag integer default 0, -- 默认值可以设为0，但COMMENT要单独写
-    create_by bigint default 0, -- 默认值可以设为0，但COMMENT要单独写
+    del_flag integer default 0, -- 0，COMMENT
+    create_by bigint default 0, -- 0，COMMENT
     create_time timestamp with time zone not null default now()
   ) TABLESPACE pg_default;
 
--- 添加注释
-comment on column public.navigation_category.del_flag is '是否删除 0:未删除 1:删除';
-comment on column public.navigation_category.create_by is '创建者';
+--
+comment on column public.navigation_category.del_flag is '0: 1:';
+comment on column public.navigation_category.create_by is '';
 
 
 CREATE TABLE IF NOT EXISTS public.web_navigation (
